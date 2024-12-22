13c6a5a88015bb237c0d09b76a686bc1bc427215
GwynWolf <chvs1312@gmail.com>
2024-12-22 16:18:41 +0200

    update class Main

--- src/main/java/com/khai/Main.java
+++ src/main/java/com/khai/Main.java
@@ -2,6 +2,9 @@ package com.khai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Order order = new Order(1, "test order");
+        System.out.println(order.getDescription());
+        OrderProcessor orderProcessor = new OrderProcessor();
+        orderProcessor.processOrder(order);
     }
 }
