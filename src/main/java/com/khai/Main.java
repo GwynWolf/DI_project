241409e37708b894d01e53032ba218fdf96e8027
GwynWolf <chvs1312@gmail.com>
2024-12-22 16:22:07 +0200

    update class Main after refactor

--- src/main/java/com/khai/Main.java
+++ src/main/java/com/khai/Main.java
@@ -3,8 +3,9 @@ package com.khai;
 public class Main {
     public static void main(String[] args) {
         Order order = new Order(1, "test order");
+        NotificationService notificationService = new NotificationService();
         System.out.println(order.getDescription());
-        OrderProcessor orderProcessor = new OrderProcessor();
+        OrderProcessor orderProcessor = new OrderProcessor(notificationService);
         orderProcessor.processOrder(order);
     }
 }
